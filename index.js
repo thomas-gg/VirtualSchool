e7dd3dde6167534bc5e83803d9f264362cf0dfb0
thomas-gg <32282600+thomas-gg@users.noreply.github.com>
2019-06-19 15:50:20 -0700

    hopefully fixed it this time

--- index.js
+++ index.js
@@ -131,11 +131,9 @@ app.post('/fileUpload', function (req, res){
       var newPath =  __dirname + '/uploadSettings/approval/mediaPreApproval/' + location + "_" + files.filetoupload.name;
       mv(oldPath, newPath, function (err) {
         if (err) throw err;
-        fileName = files.name;
-        fileName = fileName.substring(0, fileName.indexOf("."));
-        fs.writeFile(__dirname + "/uploadSettings/approval/mediaPreApproval/" + location + "_" + fileName + ".txt", caption, function(err, data) {
-          if (err) console.log(err);
-        });
+        fs.writeFile(__dirname + "/uploadSettings/approval/mediaPreApproval/" + location + "_" + files.filetoupload.name.substring(0, files.filetoupload.name.indexOf(".")) + ".txt", caption, function(err, data) {
+              if (err) console.log(err);
+          });
       });
     });
 
