78e6744cc554716ad21f3dda59ac4bb6fc88aafd
thomas-gg <32282600+thomas-gg@users.noreply.github.com>
2019-11-05 20:16:11 -0800

    adn

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ app.post('/createTeachers', function (req, res,next){
           error = 2;
           console.log(req.body.teacherNum.length);
         }
-        else if(isNaN(req.body.teacherNum.substring(0,3)) && !(req.body.teacherNum.substring(0,3) === "GYM") && !(req.body.teacherNum.substring(0,3) === "STG") && !(req.body.teacherNum.substring(0,3) === "TTR")&& !(req.body.teacherNum.substring(0,3) === "LIB")&& !(req.body.teacherNum.substring(0,3) === "DNC")&& !(req.body.teacherNum.substring(0,3) === "CON")){
+        else if(isNaN(req.body.teacherNum.substring(0,3)) && !(req.body.teacherNum.substring(0,3) === "GYM") && !(req.body.teacherNum.substring(0,3) === "STG") && !(req.body.teacherNum.substring(0,3) === "TTR")&& !(req.body.teacherNum.substring(0,3) === "LIB")&& !(req.body.teacherNum.substring(0,3) === "DNC")&& !(req.body.teacherNum.substring(0,3) === "CON")&& !(req.body.teacherNum.substring(0,3) === "ADN")){
           error = 3; //if it's NaN but is not the Gym
         }
         else if(isNaN(req.body.teacherNum.substring(0,3)) && (req.body.teacherNum.substring(0,3) === "GYM") && !(req.body.teacherNum.substring(3)==="A") && !(req.body.teacherNum.substring(3)==="B")&& !(req.body.teacherNum.substring(3)==="C")&& !(req.body.teacherNum.substring(3)==="D")){
@@ -128,6 +128,9 @@ app.post('/createTeachers', function (req, res,next){
             identi = "0009"
           }
           else if(req.body.teacherNum.substring(0,3) === "CON"){
+            identi = "0010"
+          }
+          else if(req.body.teacherNum.substring(0,3) === "ADN"){
             identi = "0010"
           }
           else{
